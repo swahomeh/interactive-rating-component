02cc01b685365fca016cb415b5eea76d8e34fead
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2022-08-05 10:57:15 +0300

    nav buttons styling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 console.log("Hello World")
+// let index = 0;
+const handleArrow = (direction) => {
+    if(direction==="l"){
+        index = 1;
+        index !== 0 ? index-1 : 2;
+
+        let pixels = -100*index;
+        suffix = 'vw';
+        console.log (pixels+' translateX('+pixels+suffix+')');
+
+        document.getElementsByClassName("slide-img")[1].style.setProperty(`transform`, 'translateX('+pixels+suffix+')');
+        document.getElementsByClassName("slide-img")[0].style.setProperty(`transform`, 'translateX('+0+suffix+')');
+    }
+    if(direction==="r"){
+        index = 0;
+        index !== 1 ? index+1 : 0;
+
+        let pixels = -100*index;
+        suffix = 'vw';
+        console.log (pixels);
+
+        document.getElementsByClassName("slide-img")[1].style.setProperty(`transform`, 'translateX('+pixels+suffix+')');
+        document.getElementsByClassName("slide-img")[0].style.setProperty(`transform`, 'translateX('+0+suffix+')');
+    }
+}
+
+// const lis = document.querySelectorAll('.rating li');
+
+// function handleClick() {
+//     const suffix = this.dataset.sizing || 'vw';
+//     document.documentElement.style.setProperty(`--$(this.name`, this.value + suffix);
+// }
+
+// lis.forEach(li => li.addEventListener('change', handleClick));
